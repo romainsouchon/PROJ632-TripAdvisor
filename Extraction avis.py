fe028cf6a0e4d989baa47bfdc7ef32f49b52ca98
Antoine Cochard <Antoine.Cochard@etu.univ-savoie.fr>
2021-02-05 10:58:09 +0100

    Update Extraction avis.py
    Tri commentaires ok !

--- Extraction avis.py
+++ Extraction avis.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
 
 URL1 = 'https://www.tripadvisor.fr/Restaurant_Review-g8309764-d968592-Reviews-Brasserie_le_Z-Chambery_Savoie_Auvergne_Rhone_Alpes.html'
 URL2 = 'https://www.tripadvisor.fr/Restaurant_Review-g8309764-d968592-Reviews-or10-Brasserie_le_Z-Chambery_Savoie_Auvergne_Rhone_Alpes.html'
@@ -8,33 +7,55 @@ URL2 = 'https://www.tripadvisor.fr/Restaurant_Review-g8309764-d968592-Reviews-or
 URL = URL1   #MODIFIER ICI
 
 page = requests.get(URL)
-uClient = uReq(URL)
-page_html = uClient.read()
-uClient.close()
-
 
 soup = BeautifulSoup(page.content, 'html.parser')
+
+#Utilisateurs
 users = soup.find_all('div',{'class' : 'info_text pointer_cursor'})
-note = soup.find_all('span',{'class' : 'ui_bubble_rating'})
-review = soup.find_all('p',{'class' : 'partial_entry'})
+
+#Notes
+notes = soup.find_all('span', {'class' : 'ui_bubble_rating'})
+
+#Ajustement notes
+review_notes = []
+for note in notes[5:15]:
+    review_notes.append(str(note)[37:39])
+
+#Commentaires            
+com_entier = soup.find_all('div', {'class' : 'ui_column is-9'})
+liste_com =[]
+for k in range(len(com_entier)):
+    com_entier_str=str(com_entier[k])
+    indice_debut = com_entier_str.find('<p class="partial_entry">') + 25
+    indice_fin = com_entier_str.find('</p></div></div><div class="prw_rup prw_reviews_stay_date_hsx" data-prwidget-init="" data-prwidget-name="reviews_stay_date_hsx">')
+    com = com_entier_str[indice_debut:indice_fin]
+    liste_com.append(com)
+
+#Dates
 date = soup.find_all('span', {'class' : 'ratingDate'})
-#print(date)
-print('Tailles avant changements')
+daterep = soup.find_all('span', {'class' : 'responseDate'})
+
+#Info_user
+info_users = soup.find_all('div', {'class' : 'member_info'})
+'''print(info_users[0])'''
+
+
+'''print('Tailles avant changements')
 print('users :', len(users))
 print('note :', len(note))
 print('review :', len(review))
 print('date :', len(date))
 
-note = note[5:15]
 print('Tailles après changements')
 print('users :', len(users))
 print('note :', len(note))
 print('review :', len(review))
-print('date :', len(date), '\n')
+print('date :', len(date), '\n')'''
 
 for i in range(0, len(users)-1):
     print (users[i].string, '\n')
-    print (note[i], '\n')
-    print (review[i].string, '\n\n')
+    print ('note :',review_notes[i], '\n')
+    print (liste_com[i], '\n')
+    print (date[i].string, '\n\n')
 
 
