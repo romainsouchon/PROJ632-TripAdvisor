bb150a8073f23f49c3888b6945fb98f7cc493ba3
Souchon <72502457+romainsouchon@users.noreply.github.com>
2021-02-05 11:23:59 +0100

    Ajout commentaire par client
    Suppression des réponses du proprio

--- Web_scraping.py
+++ Web_scraping.py
@@ -33,25 +33,32 @@ notes = soup.find_all('span', {'class' : 'ui_bubble_rating'})
 
 date = soup.find_all('span', {'class' : 'ratingDate'})
 
+#commentaire par user
+com_entier = soup.find_all('div', {'class' : 'ui_column is-9'})
+liste_com =[]
+for k in range(len(com_entier)):
+    com_entier_str=str(com_entier[k])
+    indice_debut = com_entier_str.find('<p class="partial_entry">') + 25
+    indice_fin1 = com_entier_str.find('</p></div></div><div class="prw_rup prw_reviews_stay_date_hsx" data-prwidget-init="" data-prwidget-name="reviews_stay_date_hsx">')
+    indice_fin2 = com_entier_str.find('</p></div></div><div class="prw_rup prw_reviews_inline_photos_hsx" data-prwidget-init="" data-prwidget-name="reviews_inline_photos_hsx">')
+    indice_fin=max(indice_fin1,indice_fin2)
+    com = com_entier_str[indice_debut:indice_fin]
+    liste_com.append(com)
+
 #notes des avis clean
 review_notes = []
 for note in notes[5:15]:
     review_notes.append(str(note)[37:39])
 
 for i in range(0, len(review_notes)):
-    review_notes[i] = int(review_notes[i])//10
+    review_notes[i] = int(review_notes[i])/10
     
-for i in range(0, len(users)-1):
-    print (users[i].string)
-    print (review_notes[i],'/ 5')
-    print (review[i].string,'\n\n')
-    print(date[i].string)
     
 commentaires = []   #ajout de la liste commentaires
-for i in range(0,len(users)-1):                 #un Dico = un avis
+for i in range(0,len(users)):                 #un Dico = un avis
     avis = {"pseudo" : users[i].string,         #Avec le pseudo
             "note" : review_notes[i],           #La note sur 5
-            "commentaire" : review[i].string,   #Le com
+            "commentaire" : liste_com[i],       #Le com
             "date" : date[i].string}            #La date
 
     
